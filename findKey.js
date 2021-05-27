e147fb0c63d133cb3e6c5e68ae600ce3f1f6568c
nv_twistt <mtamkee@icloud.com>
2021-05-27 11:38:56 -0600

    adding files

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const findKey = function(object, callback) {
+  //console.log(object);
+  let values = [];
+  for (let items in object) {
+    let location = object[items];
+    //console.log(location['stars']);
+    if (callback(location)) {
+      console.log(items);
+      break;
+    }
+  }
+}
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
---
+++ map.js
@@ -0,0 +1,17 @@
+const map = function(array,callback) {
+  // empty for now :)
+  // console.log('array: ', array);
+  // console.log('callback: ', callback);
+  let results = [];
+  
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log(callback(item));
+    //console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+}
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, x => x[0]);
+console.log(results1);
