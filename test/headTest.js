8000ac0d717718e5bc29b08e358fa51e504ff329
nvTwistt <mtamkee@icloud.com>
2021-05-31 18:05:17 +0000

    commit first

---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+const head = require('../head');
+const assertEqual = require('../assertEqual');
+assertEqual(head([]), 5);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+module.exports = assertEqual;
